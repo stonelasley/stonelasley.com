aced6b4742bc989e05f54840cc22f8439ec2a964
Stone C. Lasley <stone@lasley.io>
2025-10-25 18:15:06 -0600

    fix(SocialNav.tsx): fix hevy icon and URL

--- components/SocialNav.tsx
+++ components/SocialNav.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { siGithub, siInstagram, siWhatsapp } from "simple-icons";
+import { siGithub, siHevy, siInstagram, siWhatsapp } from "simple-icons";
 import { siteConfig } from "@/site.config";
 
 interface SocialLink {
@@ -34,10 +34,7 @@ const socialLinks: SocialLink[] = [
   {
     name: "Hevy",
     href: siteConfig.social.hevy,
-    icon: {
-      path: "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z",
-      hex: "000000",
-    },
+    icon: siHevy,
     ariaLabel: "Visit Stone's Hevy profile",
   },
   {
